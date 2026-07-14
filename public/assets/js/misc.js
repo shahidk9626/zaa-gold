344d3af5ceba9f74ecb9470a0dfdc1e9b4f02576
shahidkhansedar <shahidkhanasind25@gmail.com>
2026-07-14 14:33:36 +0530

    Update misc.js

--- public/assets/js/misc.js
+++ public/assets/js/misc.js
@@ -11,31 +11,31 @@
     //Active class can be hard coded directly in html file also as required
 
     function addActiveClass(element) {
-      if (current === "") {
-        //for root url
-        if (element.attr('href').indexOf("index.html") !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
+      var href = element.prop('href');
+      if (!href) return;
+
+      var cleanHref = href.split('?')[0].replace(/\/+$/, "");
+      var cleanCurrent = location.href.split('?')[0].replace(/\/+$/, "");
+
+      var isMatch = false;
+      if (cleanCurrent === cleanHref) {
+        isMatch = true;
+      } else if (cleanHref !== window.location.origin && cleanCurrent.indexOf(cleanHref + '/') === 0) {
+        isMatch = true;
+      }
+
+      if (isMatch) {
+        element.parents('.nav-item').last().addClass('active');
+        if (element.parents('.sub-menu').length) {
+          element.closest('.collapse').addClass('show');
+          element.addClass('active');
         }
-      } else {
-        //for other url
-        if (element.attr('href').indexOf(current) !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
-          if (element.parents('.submenu-item').length) {
-            element.addClass('active');
-          }
+        if (element.parents('.submenu-item').length) {
+          element.addClass('active');
         }
       }
     }
 
-    var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
     $('.nav li a', sidebar).each(function() {
       var $this = $(this);
       addActiveClass($this);
